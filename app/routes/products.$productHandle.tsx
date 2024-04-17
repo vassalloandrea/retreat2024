1e2fe7572a192311f377f1236dfe23e4f9c35f6b
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2024-04-17 09:31:41 +0200

    Get product handle and show it

--- app/routes/products.$productHandle.tsx
+++ app/routes/products.$productHandle.tsx
@@ -1,3 +1,14 @@
+import { LoaderFunctionArgs, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  return json({
+    productHandle: params.productHandle
+  })
+}
+
 export default function ProductPage() {
-  return <h1>Product detail</h1>
+  const { productHandle } = useLoaderData<typeof loader>()
+
+  return <h1>Product detail of: {productHandle}</h1>
 }
