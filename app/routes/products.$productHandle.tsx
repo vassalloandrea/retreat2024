8a87493973fdeaa2446b893c7ea400893cdd96fc
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2024-04-17 11:19:50 +0200

    Add the form

--- app/routes/products.$productHandle.tsx
+++ app/routes/products.$productHandle.tsx
@@ -43,6 +43,31 @@ export default function ProductPage() {
           </span>
         </li>
       ))}
+
+      <hr />
+
+      <Form method="post" className="mt-5 w-96">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="message"
+          >
+            Message
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            name="message"
+            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Leave feedback
+        </button>
+      </Form>
     </div>
   )
 }
